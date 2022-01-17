0d8c4d92d79b16a8414251f5bc52e37e0f03e5b0
modpage1512 <65136626+modpage1512@users.noreply.github.com>
2022-01-17 15:57:27 -0400

    Update AddButton.jsx

--- components/AddButton.jsx
+++ components/AddButton.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Add.module.css";
 const AddButton = ({ setClose }) => {
   return (
     <div onClick={() => setClose(false)} className={styles.mainAddButton}>
-      Añadir Pizza
+      Añadir Pizza a la base de datos
     </div>
   );
 };
