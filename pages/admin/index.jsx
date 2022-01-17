0f0745164d574993cae72a2d2db2625a188f8495
modpage1512 <65136626+modpage1512@users.noreply.github.com>
2022-01-17 14:49:37 -0400

    Update index.jsx

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -12,7 +12,7 @@ const Index = ({ orders, products }) => {
     console.log(id);
     try {
       const res = await axios.delete(
-        "http://localhost:3000/api/products/" + id
+        "https://pizza-restaurante-online.vercel.app/api/products/" + id
       );
       setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
     } catch (err) {
@@ -25,7 +25,7 @@ const Index = ({ orders, products }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
+      const res = await axios.put("https://pizza-restaurante-online.vercel.app/api/orders/" + id, {
         status: currentStatus + 1,
       });
       setOrderList([
@@ -128,8 +128,8 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
-  const productRes = await axios.get("http://localhost:3000/api/products");
-  const orderRes = await axios.get("http://localhost:3000/api/orders");
+  const productRes = await axios.get("https://pizza-restaurante-online.vercel.app/api/products");
+  const orderRes = await axios.get("https://pizza-restaurante-online.vercel.app/api/orders");
 
   return {
     props: {
